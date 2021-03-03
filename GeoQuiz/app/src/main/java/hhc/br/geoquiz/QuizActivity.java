f14e66aca9882e4a4ec2d386e8a92cc9256e6b89
hhc <holdn3@gmail.com>
2021-03-02 23:39:21 -0500

    chapter 2 completed

--- GeoQuiz/app/src/main/java/hhc/br/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/hhc/br/geoquiz/QuizActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +14,20 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mBtTrue;
     private Button mBtFalse;
+    private ImageButton mBtNext;
+    private ImageButton mBtPrev;
+    private TextView mTvQuestion;
+
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
+
+    private int mCurrentIndex = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +36,60 @@ public class QuizActivity extends AppCompatActivity {
 
         mBtTrue = findViewById(R.id.bt_true);
         mBtFalse = findViewById(R.id.bt_false);
+        mBtNext = findViewById(R.id.bt_next);
+        mBtPrev = findViewById(R.id.bt_prev);
+        mTvQuestion = findViewById(R.id.tv_question);
+
+        updateQuestion();
 
         mBtTrue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        "true pressed !",
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
         mBtFalse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        "false pressed !",
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+        mBtNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateQuestion();
+            }
+        });
+        mBtPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prevQuestion();
+            }
+        });
+        mTvQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateQuestion();
+            }
+        });
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        if(mQuestionBank[mCurrentIndex].isAnswerTrue() == userPressedTrue){
+            Toast.makeText(this, getString(R.string.right_toast), Toast.LENGTH_SHORT)
+                    .show();
+        } else {
+            Toast.makeText(this, getString(R.string.wrong_toast), Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    private void updateQuestion(){
+        mCurrentIndex = (++mCurrentIndex) % mQuestionBank.length;
+        mTvQuestion.setText(mQuestionBank[mCurrentIndex].getTextResId());
+    }
+    private void prevQuestion(){
+        mCurrentIndex = --mCurrentIndex >= 0 ? mCurrentIndex : mQuestionBank.length - 1;
+        mTvQuestion.setText(mQuestionBank[mCurrentIndex].getTextResId());
     }
 }
