6b9bf37896b8f9c9124d470301bcd3324cf4b154
hhc <holdn3@gmail.com>
2021-03-15 14:10:09 -0400

    ready for recycler

--- CriminalIntent/app/src/main/java/hhc/br/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/hhc/br/criminalintent/CrimeFragment.java
@@ -6,15 +6,22 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import static android.widget.CompoundButton.*;
+
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mBtDate;
+    private CheckBox mBoxSolved;
 
     @Nullable
     @Override
@@ -29,6 +36,19 @@ public class CrimeFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mBtDate = view.findViewById(R.id.crime_date);
+        mBtDate.setText(mCrime.getDate().toString());
+        mBtDate.setEnabled(false);
+
+        mBoxSolved = view.findViewById(R.id.crime_solved);
+        mBoxSolved.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
         mTitleField = view.findViewById(R.id.crime_title);
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
